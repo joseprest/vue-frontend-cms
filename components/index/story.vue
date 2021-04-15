3f0b6daed2ef4ce20b303cffc875d6ee57c8da07
Sergey P <pozhilov@gmail.com>
2021-04-16 00:21:47 +0300

    Home: Testimonials section

---
+++ components/index/story.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="story">
+    <p class="text">"{{ testimonial.text }}"</p>
+    <p class="link">
+      <ws-link
+        :title="fullStory"
+        :url="testimonial.read_more_link"
+        :is-small="true"
+        :is-halftransparent="true"
+        class="mt-25 is-size-7"
+      />
+    </p>
+    <div class="infos">
+      <span class="logo">
+        <figure class="image">
+          <ws-image :src="$getImageUrlFromCms(testimonial.logo)" alt="" />
+        </figure>
+      </span>
+      <span class="name">
+        {{ testimonial.name }}
+      </span>
+      <span class="job">
+        {{ testimonial.job }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Story',
+  props: {
+    fullStory: {
+      type: String,
+      default: 'Read more',
+    },
+    testimonial: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.story {
+  .text {
+    position: relative;
+    color: $grey;
+    font-size: 0.9em;
+    margin: 5px 10px 0;
+  }
+  .link {
+    display: block;
+    font-size: 0.8em;
+    position: absolute;
+    left: 30px;
+    bottom: 20px;
+    font-weight: bold;
+    a,
+    span {
+      display: inline-flex;
+      align-items: center;
+      svg {
+        margin-left: 5px;
+      }
+    }
+  }
+  .logo {
+    position: absolute;
+    left: -80px;
+    top: 0;
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    img,
+    div {
+      max-width: 75px;
+      max-height: 65px;
+      height: auto;
+      filter: grayscale(1);
+    }
+  }
+  .infos {
+    position: absolute;
+    bottom: 20px;
+    right: 30px;
+    border-left: 1px solid rgba(#99b2c5, 0.6);
+    padding-left: 10px;
+    display: flex;
+    flex-direction: column;
+    max-width: 150px;
+    @include widescreen-only {
+      max-width: 130px;
+    }
+    @include tablet-only {
+      max-width: 130px;
+    }
+    @include mobile {
+      max-width: 110px;
+    }
+    align-items: left;
+    .job,
+    .name {
+      color: $grey;
+      font-size: 0.65em;
+    }
+    .name {
+      margin-bottom: 3px;
+    }
+    .job {
+      font-size: 0.6em;
+      opacity: 0.6;
+      max-width: 90px;
+    }
+  }
+}
+</style>
---
+++ components/index/testimonials.vue
@@ -0,0 +1,229 @@
+<template>
+  <section class="section testimonials home">
+    <div class="container">
+      <button class="prev" @click.prevent="slidePrev">
+        <img src="@/assets/imgs/arrow.svg" alt="prev" />
+      </button>
+      <button class="next" @click.prevent="slideNext">
+        <img src="@/assets/imgs/arrow.svg" alt="next" />
+      </button>
+      <hooper v-if="showSlider" ref="carousel" :settings="hooperSettings">
+        <template v-for="testimonial in cmsData.testimonials">
+          <slide
+            v-if="!testimonial['no-testimonial']"
+            :key="testimonial.id"
+            :index="testimonial.id"
+          >
+            <div class="content-slide">
+              <story
+                :testimonial="testimonial"
+                :full-story="cmsData.read_full_title"
+              />
+            </div>
+          </slide>
+        </template>
+        <hooper-pagination slot="hooper-addons"></hooper-pagination>
+      </hooper>
+    </div>
+  </section>
+</template>
+
+<script>
+import { Hooper, Pagination as HooperPagination, Slide } from 'hooper'
+import Story from './story'
+
+export default {
+  components: {
+    Hooper,
+    Slide,
+    HooperPagination,
+    Story,
+  },
+  props: {
+    cmsData: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      carouselData: 0,
+      hooperSettings: {
+        itemsToShow: 2,
+        itemsToSlide: 1,
+        centerMode: false,
+        infiniteScroll: true,
+        keysControl: true,
+        autoPlay: false,
+        mouseDrag: true,
+        wheelControl: false,
+        breakpoints: {
+          0: {
+            itemsToShow: 1,
+          },
+          768: {
+            itemsToShow: 1,
+          },
+          1024: {
+            itemsToShow: 2,
+          },
+        },
+      },
+      showSlider: true,
+    }
+  },
+
+  methods: {
+    slidePrev() {
+      this.$refs.carousel.slidePrev()
+    },
+    slideNext() {
+      this.$refs.carousel.slideNext()
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.testimonials.home {
+  padding: 2rem 8.5rem 4rem;
+  @include touch {
+    padding: 2rem 2rem 4rem;
+  }
+  .container {
+    width: 100%;
+  }
+  button.next,
+  button.prev {
+    color: $white;
+    background: $blue-medium;
+    appearance: none;
+    border: 0;
+    border-radius: 70%;
+    width: 36px;
+    height: 36px;
+    position: absolute;
+    top: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: opacity 0.2s linear;
+    &:hover {
+      opacity: 0.8;
+    }
+    img {
+      margin-right: 3px;
+    }
+    @include touch {
+      display: none;
+    }
+  }
+  button.prev {
+    left: -50px;
+  }
+  button.next {
+    right: -50px;
+    transform: rotate(180deg);
+  }
+  .hooper {
+    width: 100%;
+    height: 210px;
+    @include touch {
+      height: auto;
+    }
+    @include mobile {
+      border-bottom: 1px solid rgba($blue-dark, 0.1);
+    }
+  }
+  .hooper-slide {
+    position: relative;
+    border-radius: 10px;
+    border: 6px solid $white;
+    color: $white;
+    display: flex;
+    padding: 20px;
+    border-radius: 20px;
+    background: linear-gradient(180deg, #ffffff 0%, #f5f7f8 100%);
+    @include mobile {
+      border: 0;
+    }
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 1;
+      border-radius: inherit;
+      border: 1px solid #bcd2e4;
+      opacity: 0.4;
+    }
+    .content-slide {
+      z-index: 2;
+      display: flex;
+      align-items: top;
+      flex-direction: column;
+      .text {
+        font-size: 0.8em;
+        margin: 5px 10px;
+      }
+      .link {
+        z-index: 1;
+      }
+      .infos {
+        z-index: 0;
+      }
+      @include touch {
+        display: block;
+        .text {
+          margin: 0;
+          display: block;
+        }
+        .link {
+          position: relative;
+          display: block;
+          left: inherit;
+          bottom: inherit;
+          margin-top: 15px;
+        }
+        .infos {
+          position: relative;
+          display: block;
+          bottom: inherit;
+          right: inherit;
+          float: right;
+          margin-top: 20px;
+          max-width: 100%;
+          .logo,
+          .name,
+          .job {
+            position: relative;
+            display: block;
+            left: inherit;
+            top: inherit;
+            width: 100%;
+            max-width: 100%;
+          }
+          .logo {
+            img {
+              max-width: 90px;
+              max-height: 80px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .hooper-pagination {
+    bottom: -30px;
+    .hooper-indicator {
+      background: #bcd2e4;
+    }
+    .hooper-indicator.is-active {
+      background: $green;
+    }
+  }
+}
+</style>
