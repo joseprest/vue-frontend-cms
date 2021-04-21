6737158c9fdf8daae57c3aec3152707e878e37e0
Sergey P <pozhilov@gmail.com>
2021-04-21 16:28:03 +0300

    fix for AWS: plugin getImageUrlFromCms returns correct url

--- plugins/getImageUrlFromCms.js
+++ plugins/getImageUrlFromCms.js
@@ -15,7 +15,9 @@ export default ({ app }, inject) => {
     }
 
     const finalSource = image.url
-    return `${process.env.CMS_URL}${finalSource}`
+    return finalSource.substr(0, 5) === 'https'
+      ? finalSource
+      : `${process.env.CMS_URL}${finalSource}`
     // return `http://localhost:1337${finalSource}`
   })
 }
