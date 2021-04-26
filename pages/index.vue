fa4bb16b6c959425a98063cc4159bfe6d4f51bb1
Sergey P <pozhilov@gmail.com>
2021-04-26 10:44:01 +0300

    homepage renders dynamic zones

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,13 @@
 <template>
   <div v-if="Object.keys(cmsData).length > 0">
-    <index-header :cms-data="cmsData.header" :navbar-data="cmsData.navbar" />
-    <index-upgrade-bms :cms-data="cmsData.upgrade" />
-    <index-collect-unify-bms-data :cms-data="cmsData.unify" />
-    <index-plug-and-play :cms-data="cmsData.plug_play" />
-    <index-connect-all :cms-data="cmsData.connect" />
-    <customer-stories :cms-data="cmsData.customer_stories" />
-    <index-how-it-works :cms-data="cmsData.how_it_works" />
-    <index-get-demo :cms-data="cmsData.get_demo" />
-    <index-clients
-      :cms-data="cmsData.clients"
-      :clients="cmsData.clients_logos"
-    />
-    <index-testimonials :cms-data="cmsData.testimonials" />
-    <index-integrations :cms-data="cmsData.Integrations" />
-    <index-improving-simple :cms-data="cmsData.Improving" />
+    <template v-for="comp in cmsData.body">
+      <component
+        :is="$getComponentFromCms(comp.__component)"
+        :key="`body-${comp.__component}-${comp.id}`"
+        :clients="cmsData.clients_logos"
+        :cms-data="comp"
+      />
+    </template>
     <app-footer />
   </div>
 </template>
@@ -33,12 +26,12 @@ export default {
 
   async created() {
     const content = await axios.all([
-      axios.get(this.$getUrlFromCms('/home?locale.language=en')),
+      axios.get(this.$getUrlFromCms('/homepage')),
       axios.get(this.$getUrlFromCms('/testimonials')),
       axios.get(this.$getUrlFromCms('/clients-logos')),
     ])
     this.cmsData = {
-      ...content[0].data[0],
+      ...content[0].data,
       testimonials: content[1].data,
       clients_logos: content[2].data.logos,
     }
