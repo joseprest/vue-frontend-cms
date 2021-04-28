b734597c808317d8e3f025f6b4808943826ed17b
Sergey P <pozhilov@gmail.com>
2021-04-28 10:08:29 +0300

    folder with index components renamed and footer-list component renamed to meet strapi naming conventions

--- plugins/getComponentFromCms.js
+++ plugins/getComponentFromCms.js
@@ -1,14 +1,5 @@
 export default ({ app }, inject) => {
   inject('getComponentFromCms', (...parameters) => {
-    const cmsComponent = parameters[0]
-    switch (cmsComponent) {
-      case 'footer.links':
-        return 'footer-list'
-
-      default:
-        return cmsComponent
-          .replace('.', '-')
-          .replace('index-components', 'index')
-    }
+    return parameters[0].replace('.', '-')
   })
 }
