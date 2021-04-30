cdb1c6893018395f98ee07bcbae9c5ccafd21990
Sergey P <pozhilov@gmail.com>
2021-04-30 15:57:30 +0300

    general page container component

---
+++ components/page-container.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header class="section">
+      <navbar :home="false" />
+    </header>
+    <page-header :cms-data="headerData" />
+    <slot />
+    <app-footer />
+  </div>
+</template>
+<script>
+export default {
+  name: 'PageContainer',
+  props: {
+    headerData: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style scoped>
+header {
+  padding-top: 0;
+}
+</style>
