c21990a22f5abceacf15c4010a21aac948757bda
Sergey P <pozhilov@gmail.com>
2021-05-13 21:39:33 +0300

    /resources page

---
+++ pages/resources/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <page-container
+    v-if="Object.keys(cmsData).length > 0"
+    :cms-data="cmsData.page_title"
+  >
+    <section class="section main" style="min-height: 80vh">
+      <div class="container">
+        <ws-title
+          :title="cmsData.title"
+          :description="cmsData.text"
+          align="left"
+        />
+        <div
+          v-for="section in cmsData.resources"
+          :key="`resources${section.id}`"
+        >
+          <p class="title">{{ section.title }}</p>
+          <div class="columns is-multiline mb-25 integration">
+            <div
+              v-for="btn in section.buttons"
+              :key="`res${section.id}-${btn.id}`"
+              class="column is-4-widescreen is-6-desktop is-6-tablet is-12-mobile"
+            >
+              <button-extended :cms-data="btn" :color="section.color" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </page-container>
+</template>
+
+<script>
+import ButtonExtended from '@/components/button-extended.vue'
+export default {
+  name: 'Resources',
+  nuxtI18n: {
+    paths: {
+      en: '/resources',
+      fr: '/ressources',
+      de: '/ressourcen',
+    },
+  },
+  components: {
+    ButtonExtended,
+  },
+  async asyncData({ i18n, $axios, $getUrlFromCms }) {
+    const content = await $axios.get(
+      $getUrlFromCms('/page-resources?_locale=' + i18n.localeProperties.iso)
+    )
+    return {
+      cmsData: { ...content.data },
+    }
+  },
+
+  head() {
+    return {
+      title: this.cmsData.meta.title,
+      meta: [
+        {
+          vmid: 'description',
+          name: 'description',
+          content: this.cmsData.meta.description,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.section.main {
+  background-image: linear-gradient(to bottom, white, #f0f5f7);
+}
+
+.title {
+  width: 100%;
+  max-width: 100%;
+  font-size: $size-4;
+  line-height: 1.75em;
+  margin-bottom: 15px;
+  font-weight: $weight-medium;
+  color: rgba($gray-dark, 0.8);
+
+  @include mobile {
+    font-size: $size-4;
+  }
+}
+</style>
