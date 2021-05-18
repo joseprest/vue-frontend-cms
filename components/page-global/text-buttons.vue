8677c50a4c3e595786b2e3d51bf4944eaac50c9f
Sergey P <pozhilov@gmail.com>
2021-05-18 08:41:14 +0300

    2 universal components for pages: text block + set of buttons, rich text with title + image with caption. Both comps are used on iot-security page

---
+++ components/page-global/text-buttons.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div
+      v-if="cmsData.text"
+      class="rich-text"
+      :class="`rich-text-${cmsData.text_style}`"
+      v-html="$richTextImageUrls($md.render(cmsData.text))"
+    />
+    <p class="buttons is-flex align-center mt-25">
+      <!-- TODO: button should force demo model to pop up -->
+      <ws-button
+        v-for="btn in cmsData.buttons"
+        :key="`text-btn-${btn.id}`"
+        :url="btn.file ? btn.file.url : btn.url"
+        :is-inverted="btn.inverted"
+        class="mt-20 mx-5"
+      >
+        {{ btn.title }}
+      </ws-button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextButtons',
+  props: {
+    cmsData: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/page-global/text-titled-image.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="text-titled-image">
+    <ws-title v-if="cmsData.title" :title="cmsData.title" />
+    <div
+      v-if="cmsData.text"
+      class="rich-text"
+      v-html="$richTextImageUrls($md.render(cmsData.text))"
+    />
+    <div class="image-container">
+      <figure class="image has-text-centered">
+        <ws-image
+          :src="$getImageUrlFromCms(cmsData.image)"
+          :alt="cmsData.image.alternativeText"
+        />
+        <small>{{ cmsData.image.caption }}</small>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextTitledImage',
+  props: {
+    cmsData: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$max-height-image: 650px;
+
+.image-conteiner {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2rem;
+  max-height: $max-height-image;
+
+  .image {
+    img {
+      width: unset !important;
+      max-height: $max-height-image;
+      max-width: 100%;
+    }
+  }
+}
+</style>
