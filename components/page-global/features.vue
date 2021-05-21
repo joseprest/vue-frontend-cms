5e1784fb64fe21ba2aabc0d10901bdcbe587fab9
Sergey P <pozhilov@gmail.com>
2021-05-21 14:24:47 +0300

    Components in Page-global

---
+++ components/page-global/features.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <ws-title v-if="cmsData.title" :title="cmsData.title" />
+    <div
+      v-if="cmsData.image"
+      v-scroll-reveal="{ delay: 0, reset: false }"
+      class="product"
+    >
+      <ws-image
+        :src="$getImageUrlFromCms(cmsData.image)"
+        alt="cmsData.image.alternativeText"
+      />
+    </div>
+    <list-items :items="cmsData.features" />
+
+    <div v-if="cmsData.buttons" class="cta">
+      <ws-button
+        v-for="btn in cmsData.buttons"
+        :key="`cta${btn.id}`"
+        :title="btn.title"
+        :url="btn.url"
+        class="mt-35 mx-5"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ListItems from './list-items.vue'
+
+export default {
+  components: {
+    ListItems,
+  },
+  props: {
+    cmsData: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  @include touch {
+    display: flex;
+  }
+}
+.product {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 60px;
+  img {
+    max-width: 350px;
+    height: auto;
+  }
+  @include touch {
+    margin-top: 20px;
+    img {
+      max-width: 300px;
+    }
+  }
+}
+.cta {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ components/page-global/list-items.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="columns is-multiline">
+      <div v-for="item in items" :key="item.id" class="column is-6 is-12-touch">
+        <ul class="list-item">
+          <ws-list-item check-type="check">
+            <div class="title has-text-weight-semibold">
+              {{ item.title }}
+            </div>
+            <div>
+              {{ item.text }}
+            </div>
+            <!-- TOOO add another ws-link component with v-if, for popup/gotolink -->
+            <ws-link v-if="item.button" :link="item.button.url" class="mt-25">
+              <!--              @click.native="requestDemo('box')"-->
+              {{ item.button.title }}
+            </ws-link>
+          </ws-list-item>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  methods: {
+    requestDemo(product) {
+      // TODO demo popup
+      this.$gtm.trackEvent({
+        event: 'uaevent',
+        category: 'navigation',
+        action: 'get a demo',
+        label: 'benefits Integrators',
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.list-item {
+  li {
+    font-weight: 600;
+    div {
+      font-weight: normal;
+    }
+    @include touch {
+      a {
+        margin-top: 0 !important;
+      }
+    }
+    a.span {
+      font-weight: inherit;
+      display: inherit;
+    }
+  }
+  .title {
+    font-size: 1.35rem;
+    margin-bottom: 14px;
+  }
+  @include touch {
+    li.item-list {
+      font-size: inherit;
+    }
+  }
+}
+</style>
