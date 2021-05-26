177fd0b61e7b2b95e43d568bfda038a3ff42ab68
Sergey P <pozhilov@gmail.com>
2021-05-26 09:03:11 +0300

    first 2 components for pricing pages

---
+++ components/page-global/box-subscription.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <ws-title
+      v-if="cmsData.title"
+      :title="cmsData.title"
+      :description="cmsData.subtitle"
+    />
+
+    <div class="columns is-vcentered is-centered advantages">
+      <div
+        v-for="item in cmsData.features"
+        :key="`subscr-feature${item.id}`"
+        class="column is-4"
+      >
+        <div
+          v-scroll-reveal="{ delay: 0, reset: false }"
+          class="action-pricing"
+        >
+          <div class="icon">
+            <img
+              :src="$getImageUrlFromCms(item.image)"
+              :alt="item.image.alternativeText"
+              class="icon"
+            />
+          </div>
+          <div class="title">
+            {{ item.title }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cmsData: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.advantages {
+  margin-left: 15%;
+  margin-right: 15%;
+  @include desktop {
+    margin-left: 8%;
+    margin-right: 8%;
+  }
+  @include tablet-only {
+    margin-left: 8%;
+    margin-right: 8%;
+  }
+  @include touch {
+    margin-left: 0;
+    margin-right: 0;
+    margin-top: 20px;
+  }
+  .box {
+    font-weight: 600;
+    text-align: center;
+  }
+}
+
+.action-pricing {
+  background: $white;
+  padding: 5px;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+  border-radius: 10px;
+  box-shadow: 0 16px 25px rgba(24, 69, 101, 0.06);
+  .icon {
+    height: 100px;
+    width: 100%;
+    @include mobile {
+      margin-top: 10px;
+    }
+  }
+  .title {
+    font-size: 1em;
+    text-align: center;
+    padding: 0 20px;
+    line-height: 1.5rem;
+    height: 6rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    @include mobile {
+      height: auto;
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+  }
+}
+.icon {
+  max-height: 75px;
+}
+</style>
---
+++ components/page-global/specs.vue
@@ -0,0 +1,163 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-8">
+        <ws-title
+          v-if="cmsData.title"
+          :title="cmsData.title"
+          :description="cmsData.subtitle"
+          align="left"
+        />
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column img-container">
+        <ws-image
+          v-if="cmsData.image"
+          :src="$getImageUrlFromCms(cmsData.image)"
+          :alt="cmsData.image.alternativeText"
+        />
+      </div>
+      <div class="column box-info">
+        <div class="columns">
+          <div class="column is-7">
+            <ul class="box-list-items">
+              <ws-list-item
+                v-for="item in cmsData.features"
+                :key="`feature${item.id}`"
+                check-type="check"
+              >
+                {{ item.title }}
+                <div v-if="item.text" class="has-text-weight-normal">
+                  {{ item.text }}
+                </div>
+              </ws-list-item>
+            </ul>
+          </div>
+          <div class="column is-8">
+            <ul class="has-text-dark2">
+              <li
+                v-for="item in cmsData.features_col_2"
+                :key="`feature2-${item.id}`"
+                class="has-text-weight-bold"
+              >
+                {{ item.title }}
+                <div v-if="item.text" class="has-text-weight-normal">
+                  {{ item.text }}
+                </div>
+              </li>
+              <li>
+                <ws-link
+                  v-if="cmsData.full_spec_button"
+                  :title="cmsData.full_spec_button.title"
+                  :url="cmsData.full_spec_button.url"
+                  class="mt-25"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-if="cmsData.cta_button"
+      class="columns is-vcentered has-text-centered mb-15"
+    >
+      <div class="column has-text-centered-mobile">
+        <!-- TODO: popup @click="requestSales" -->
+        <ws-button :url="cmsData.cta_button.url">
+          {{ cmsData.cta_button.title }}
+        </ws-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cmsData: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+li.item-list {
+  font-weight: 600;
+}
+.button {
+  padding-left: 1.5em;
+  padding-right: 1.5em;
+  line-height: 1.1em !important;
+  span {
+    white-space: break-spaces;
+    text-align: left;
+  }
+}
+.button.link {
+  display: inline-block;
+  text-align: left;
+}
+
+.box-info {
+  @include mobile {
+    padding-top: 0;
+  }
+  order: 1;
+  a {
+    margin-left: 0;
+  }
+}
+
+.column.price {
+  padding-left: 0;
+  @include mobile {
+    padding-left: 0.75rem;
+  }
+}
+
+.img-container {
+  order: 2;
+
+  @include touch {
+    padding-bottom: 0;
+    padding-top: 0;
+    display: block;
+  }
+
+  @include desktop {
+    transform: translateY(-130px);
+    margin-bottom: -130px;
+  }
+
+  img {
+    width: 70%;
+
+    @include desktop {
+      margin-left: 160px;
+      margin-top: -90px;
+    }
+
+    @include tablet-only {
+      margin-left: 35%;
+      margin-top: -40px;
+    }
+
+    @include mobile {
+      width: 60%;
+      margin-left: 18%;
+      margin-top: 0;
+    }
+  }
+}
+@include mobile {
+  .ws-title {
+    margin-bottom: 0 !important;
+  }
+}
+</style>
