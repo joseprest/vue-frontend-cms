53bf47bc64000c0afa2d73bf649c9911918a0969
Sergey P <pozhilov@gmail.com>
2021-06-04 20:26:51 +0300

    Homepage: GetDemo component with ws-button

--- components/index-components/get-demo.vue
+++ components/index-components/get-demo.vue
@@ -3,13 +3,7 @@
     <div class="columns is-vcentered">
       <div class="column is-12">
         <p class="buttons">
-          <a
-            v-if="cmsData !== null"
-            class="button is-success has-text-weight-semibold has-shadow btn-big is-uppercase"
-          >
-            <!-- @click="requestDemo('box')" -->
-            {{ cmsData.button_text }}
-          </a>
+          <ws-button :cms-data="cmsData.button" />
         </p>
       </div>
     </div>
@@ -17,28 +11,14 @@
 </template>
 
 <script>
-// import { EventBus } from "@/event-bus";
-
 export default {
   name: 'IndexGetDemo',
 
   props: {
     cmsData: {
       type: Object,
-      default: null,
+      default: () => ({}),
     },
-  },
-
-  methods: {
-    // requestDemo(product) {
-    // 	this.$gtm.trackEvent({
-    // 		event: "uaevent",
-    // 		category: "navigation",
-    // 		action: "get a demo",
-    // 		label: "body"
-    // 	});
-    // 	EventBus.$emit("openRequestDemo", product);
-    // }
   },
 }
 </script>
