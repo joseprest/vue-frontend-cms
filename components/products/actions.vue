26d2bf0e07087540f7ff90ff96a7102f30865d05
Sergey P <pozhilov@gmail.com>
2021-06-07 23:31:02 +0300

    Products/Box Actions component: differend delays in Actions component

--- components/products/actions.vue
+++ components/products/actions.vue
@@ -1,48 +1,48 @@
 <template>
   <div>
-      <ws-title :title="cmsData.title" />
-      <div class="columns is-multiline">
-        <div
-          v-for="action in cmsData.content"
-          :key="`action${action.id}`"
-          class="column is-3-widescreen is-6-desktop is-6-tablet is-12-mobile"
-        >
-          <action-card v-scroll-reveal="{ delay: 0, reset: false }">
-            <template slot="icon">
-              <div class="icon">
-                <img :src="$getImageUrlFromCms(action.icon)" alt="" />
-              </div>
-            </template>
-            <template slot="title">
-              {{ action.title }}
-            </template>
-            <template slot="elements">
-              <ul class="list-item">
-                <ws-list-item
-                  v-for="element in action.elements"
-                  :key="`element${element.id}`"
-                  check-type="check-blue"
-                >
-                  <nuxt-link v-if="element.link" :to="element.link">
-                    {{ element.text }}
-                  </nuxt-link>
-                  <span v-else>{{ element.text }}</span>
-                </ws-list-item>
-              </ul>
-            </template>
-          </action-card>
-        </div>
-      </div>
-      <div class="buttons mt-35">
-        <nuxt-link
-          class="button is-success has-text-weight-semibold has-shadow btn-big is-uppercase"
-          :class="{ 'is-inverted': cmsData.button.inverted }"
-          :to="cmsData.button.url"
-        >
-          {{ cmsData.button.title }}
-        </nuxt-link>
+    <ws-title :title="cmsData.title" />
+    <div class="columns is-multiline">
+      <div
+        v-for="(action, index) in cmsData.content"
+        :key="`action${action.id}`"
+        class="column is-3-widescreen is-6-desktop is-6-tablet is-12-mobile"
+      >
+        <action-card v-scroll-reveal="{ delay: index * 400, reset: false }">
+          <template slot="icon">
+            <div class="icon">
+              <img :src="$getImageUrlFromCms(action.icon)" alt="" />
+            </div>
+          </template>
+          <template slot="title">
+            {{ action.title }}
+          </template>
+          <template slot="elements">
+            <ul class="list-item">
+              <ws-list-item
+                v-for="element in action.elements"
+                :key="`element${element.id}`"
+                check-type="check-blue"
+              >
+                <nuxt-link v-if="element.link" :to="element.link">
+                  {{ element.text }}
+                </nuxt-link>
+                <span v-else>{{ element.text }}</span>
+              </ws-list-item>
+            </ul>
+          </template>
+        </action-card>
       </div>
     </div>
+    <div class="buttons mt-35">
+      <nuxt-link
+        class="button is-success has-text-weight-semibold has-shadow btn-big is-uppercase"
+        :class="{ 'is-inverted': cmsData.button.inverted }"
+        :to="cmsData.button.url"
+      >
+        {{ cmsData.button.title }}
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script>
