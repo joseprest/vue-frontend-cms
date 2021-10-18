834fb02eecf92fcc457b2fd9bc186491b2a6f79f
Murilo Boareto Delefrate <murilobd@gmail.com>
2021-10-18 16:04:14 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -188,7 +188,7 @@ export default {
   },
 
   env: {
-    CMS_URL: process.env.CMS_URL || 'https://strapi.s.wattsense.com',
+    CMS_URL: process.env.CMS_URL || 'https://strapi.wattsense.com',
     API_ENDPOINT:
       process.env.API_ENDPOINT || 'https://console.wattsense.com/api/website/',
   },
